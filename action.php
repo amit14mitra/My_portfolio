40bbca00f0770681abebc7329b62aba65f122d3e
Amit Kumar Mitra <amit14mitra@gmail.com>
2020-06-25 20:01:22 +0530

    change done

---
+++ action.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="text-align:center;">
+        <p>your response has been submitted<br></p>
+        <a style="justify-content:center;text-align:center;text-decoration:underline;" href="mysite.html" >Go Back</a>
+    </div>    
+</body>
+</html>
+
+
+
+<?php
+
+    // $email=$_GET['email'];
+    // $name=$_GET['name'];
+    // $text=$_GET['text'];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        echo "work done";
+    }
+
+    
+    //echo $name."<br> ".$email."<br> ".$text;
+?>
+
+
+
+
+
